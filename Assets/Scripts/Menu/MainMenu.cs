07a883d40236cd1e51adfaefb0cc8c09208e933f
Yvon Morice <yvon.morice@epita.fr>
2022-06-15 01:22:53 +0200

    added particles, prefab variants for targets

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,12 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayShootingRange()
+    {
+        SceneManager.LoadScene(2);
     }
 
     public void QuitGame()
