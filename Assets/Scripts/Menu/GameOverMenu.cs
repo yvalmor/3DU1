24f26d3733e9b3eacf3d607717a9910cec7c8a93
Souleymane Sentici <souleymanesentici@live.com>
2022-06-15 20:42:05 +0200

    added gameOver menu

---
+++ Assets/Scripts/Menu/GameOverMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void GoToMenu(){
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void GoToGame(){
+        SceneManager.LoadScene("BaseGame");
+    }
+}
